056d687c2b2b7555013160c02ed5479cb924d85b
Sumit Aggarwal <sumitaggarwal3_14@outlook.com>
2021-01-18 00:38:17 +0530

    Getting started with Templates

--- django_basics/1/my_first_project/HelloWorld/views.py
+++ django_basics/1/my_first_project/HelloWorld/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	response = HttpResponse("Hello World")
+	developed_by = "Sumit Aggarwal"
+	mentors = ["Pramod","Rajni","Umesh","Sumit","Mohit"]
+	context = {
+	"developer" : developed_by,
+	"mentors" : mentors
+	}
+	# response = HttpResponse("Hello World")
+	response = render(request,"index.html",context)
 	return response
