b2c70e31fd68cf417b8baf5abd82d79ac56ca568
Sumit Aggarwal <sumitaggarwal3_14@outlook.com>
2021-02-05 02:17:39 +0530

    different fields in models

--- intro_to_models_in_django/model_playground/main/models.py
+++ intro_to_models_in_django/model_playground/main/models.py
@@ -3,5 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
+	GENDER = (
+		('f',"Female"),
+		('m',"Male"),
+		('u',"Undesclosed")
+		)
 	name = models.CharField(max_length=100)
-	roll_number = models.IntegerField()
+	roll_number = models.IntegerField(unique = True)
+	address = models.TextField(null = True)
+	phone_number = models.CharField(max_length = 15, null = True, blank = True)
+	email = models.EmailField(null = True)
+	gender = models.CharField(max_length = 1, choices = GENDER, null = True)
+
+def _str_(self):
+	return self.name
