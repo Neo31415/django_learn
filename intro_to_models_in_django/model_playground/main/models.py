4884b149517d7714a6046055e4d9127bd64d090e
Sumit Aggarwal <sumitaggarwal3_14@outlook.com>
2021-02-09 22:42:47 +0530

    Validators in models (Django core validators)

--- intro_to_models_in_django/model_playground/main/models.py
+++ intro_to_models_in_django/model_playground/main/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.core.validators import (
+EmailValidator,
+MaxValueValidator,
+MinValueValidator,
+URLValidator,
+validate_slug
+	)
+
 
 # Create your models here.
 
@@ -12,8 +20,10 @@ class Student(models.Model):
 	roll_number = models.IntegerField(unique = True)
 	address = models.TextField(null = True)
 	phone_number = models.CharField(max_length = 15, null = True, blank = True)
-	email = models.EmailField(null = True)
+	email = models.CharField(max_length = 100, null = True, validators=[EmailValidator("Invalid Email Adderss")])
 	gender = models.CharField(max_length = 1, choices = GENDER, null = True)
+	age = models.IntegerField(null=True, validators=[MaxValueValidator(150),MinValueValidator(25)])
+	slug = models.CharField(max_length = 100, null=True, validators=[validate_slug])
 
-def _str_(self):
-	return self.name
+	def __str__(self):
+		return self.name
